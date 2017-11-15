62874e50dbcc70c96a5cfec47e3620d5ac64892e
Kourken Kasbarian <kourkenk@outlook.com>
2017-11-15 23:33:11 +0400

    Complete Redesign of my portfolio
    I redesigned my portfolio from the grounds up to fix bugs, clean lots of unecessary code, less code and make it even more minimal etc.

--- README.md
+++ README.md
@@ -1,32 +1,30 @@
 # Kourken Kasbarian's Official Portfolio
 
-This is my official portfolio website, where you'll find my portfolio, works, websites and more...
+This Is My Official Portfolio Website, Where You'll Find My Portfolio, Works, Websites and More...
 
-The 1.8 Version is up and running, but it'll be updated with new contents, projects, site redesigns, under-the-hood fixes and more in the coming versions.
+The 1.7 Version is Up and Running, But It'll be Updated With New Contents, Projects, Site Redesigns, Under-The-Hood Fixes and More in the Coming Versions.
 
-My official portfolio website: [Kourken Kasbarian](https://kkasbarian.github.io/)
+My Official Portfolio Website: [Kourken Kasbarian](https://kkasbarian.github.io/)
 
-**Note**: A Blog coming not-so-soon!
+**Note**: A Blog Coming Not-So-Soon!
 
 ## Versions and Changes:
 
 1.0v: Initial Release, with lots of issues and bugs.
 
-1.1v: First update to the site, fixed some layout issues, tidied some CSS.
+1.1v: First Update to the Site, Fixed some Layout Issues, Tidied some CSS.
 
-1.2v: Second update to the site, fixed portfolio buttons issue, made some small bug fixes and bhanges.
+1.2v: Second Update to the Site, Fixed Portfolio Buttons Issue, Made some small Bug Fixes and Changes.
 
-1.3v: Third update to the site, changed some colors and layout redesigns. some buttons style changes. small footer changes.
+1.3v: Third Update to the Site, Changed some colors and layout redesigns. Some Buttons Style Changes. Small Footer Changes.
 
-1.4v: Fourth update to the site, added intro loader animation while the page loads and animated some elements on scroll.
+1.4v: Fourth Update to the Site, Added Intro Loader Animation while the Page loads and Animated some Elements on Scroll.
 
-1.5v: Fifth update to the site, A huge update, where I changed the form submission request from Formspree to a better and convenient option which doesn't even need a server or backend to be used. There is also an AJAX response when a form is submitted (So no redirects, but wait till the message shows, it could take time, so don't send a form twice) and spam prevention is (hopefully) there too. Finally, I deleted the local Saved Font Awesome folder and instead added an External Link in my HTML.
+1.5v: Fifth Update to the Site, A Huge Update, where I changed the Form Submission Request from Formspree to a better and convenient option which doesn't even need a server or backend to be used. There is also an AJAX Response when a Form is Submitted (So No Redirects) and Spam prevention is (hopefully) there too. Finally, I deleted the Local Saved Font Awesome Folder and instead added an External Link in my HTML.
 
-1.6v: Sixth update to the site, I changed the layout of the 'Some Skills' section from stacking horizontally to stacking vertically of the icons. Changed the text of the 'About' section and made it more professional. Finally, I added few more social links on the footer.
+1.6v: Sixth Update to the Site, I changed the Layout of the 'Some Skills' section from stacking horizontally to stacking vertically of the icons. Changed the text of the 'About' section and made it more professional. Finally, I added few more social links on the Footer.
 
-1.7: Seventh and a small update, I added a "Last Updated" section at the bottom of the page.
-
-1.8: Eigth and a critical update, I fixed the navigational buttons not-working at-all issue.
+1.7: Seventh and a small update, I added a "Last Updated" section at the bottom of the page and made the year at the footer change automatically.
 
 ## Feedback:
 
--- js/main.js
+++ js/main.js
@@ -1,162 +1,79 @@
-// Responsive nav menu with hamburger menu on small-sized screens
+/* Intro Loader Animation function */
+$(document).ready(function() {
+    $("#loader").introLoader({
+        animation: {
+            name: 'simpleLoader',
+            options: {
+                exitFx:'slideUp',
+                ease: "easeInOutCirc",
+                style: 'light',
+                delayBefore: 1000,
+                exitTime: 500
+            }
+        },    
 
-function responsiveMobileMenu() {
-	$('.nav').each(function() {
-
-
-
-		// $(this).children('ul').addClass('nav-menu'); // mark main menu list
-
-
-		var $style = $(this).attr('nav-menu-style'); // get menu style
-		if (typeof $style == 'undefined' || $style == false) {
-			$(this).addClass('yoga'); // set yoga style if style is not defined
-		} else {
-			$(this).addClass($style);
-		}
-
-
-		/* 	width of menu list (non-toggled) */
-
-		var $width = 0;
-		$(this).find('ul li').each(function() {
-			$width += $(this).outerWidth();
-		});
-
-		// if modern browser
-
-		if ($.support.leadingWhitespace) {
-			// $(this).css('max-width', $width * 1.2 + 'px');
-		}
-		// 
-		else {
-			$(this).css('width', $width * 1.2 + 'px');
-		}
-
-	});
-}
-
-function getMobileMenu() {
-
-	/* 	build toggled dropdown menu list */
-
-	$('.nav').each(function() {
-		var menutitle = $(this).attr("nav-menu-title");
-		if (menutitle == "") {
-			menutitle = "";
-		} else if (menutitle == undefined) {
-			menutitle = "";
-		}
-		var $menulist = $(this).children('.nav-menu').html();
-		var $menucontrols = "<div class='nav-toggled-controls'><div class='nav-toggled-title'>" + menutitle + "</div><div class='nav-button'><span>&nbsp;</span><span>&nbsp;</span><span>&nbsp;</span></div></div>";
-		$(this).prepend("<div class='nav-toggled nav-closed'>" + $menucontrols + "<ul>" + $menulist + "</ul></div>");
-
-	});
-}
-
-function adaptMenu() {
-
-	/* 	toggle menu on resize */
-
-	$('.nav').each(function() {
-		// var $width = $(this).css('max-width');
-		// $width = $width.replace('px', ''); 
-		var $width = 768;
-		if ($(this).parent().width() < $width * 1.05) {
-			$(this).children('.nav-menu').hide(0);
-			$(this).children('.nav-toggled').show(0);
-		} else {
-			$(this).children('.nav-menu').show(0);
-			$(this).children('.nav-toggled').hide(0);
-		}
-	});
-
-}
-
-$(function() {
-
-	responsiveMobileMenu();
-	getMobileMenu();
-	adaptMenu();
-
-	/* slide down mobile menu on click */
-
-	$('.nav-toggled, .nav-toggled .nav-button').click(function() {
-		if ($(this).is(".nav-closed")) {
-			$(this).find('> ul').stop().show(300);
-			$(this).removeClass("nav-closed");
-		} else {
-			$(this).find(' > ul').stop().hide(300);
-			$(this).addClass("nav-closed");
-		}
-
-	});
-
+        spinJs: {
+            lines: 13,
+            length: 20,
+            width: 5,
+            radius: 25,
+            corners: 1,
+            color: '#000',
+        }
+    });
 });
-/* 	hide mobile menu on resize */
-$(window).resize(function() {
-	adaptMenu();
+/* End Intro Loader Animation function */
+
+/* Fixed navigation on scroll function */
+
+$(document).ready(function() {
+    $('#nav').scrollToFixed();
 });
 
 $(document).ready(function() {
-	//test for touch events support and if not supported, attach .no-touch class to the HTML tag.
-	if (!("ontouchstart" in document.documentElement)) {
-		document.documentElement.className += " no-touch";
-	} else {
-		document.documentElement.className += " touch";
-	}
-
-
-	var $submenus = $('.nav.yoga .nav-menu li > ul');
-	$submenus.hide();
-	if ($('html').hasClass('no-touch')) {
-		$submenus.parent().hover(function() {
-			$(this).find('> ul').slideToggle('fast');
-		});
-	} else {
-		$submenus.parent().click(function(e) {
-			
-			$(this).find('> ul').slideToggle('fast');
-			e.preventDefault();
-			e.stopPropagation();
-		});
-	}
-
-	$submenus.parent().addClass('has-children');
-
-
-	var $submenusSm = $('.nav.yoga .nav-toggled li > ul');
-	$submenusSm.hide();
-	$submenusSm.parent().prepend('<div class="clicker"></div>');
-	$submenusSm.parent().children('a').addClass('toggle-sm');
-	var clicker = $('.clicker');
-	clicker.on('click', function(e){
-		e.stopPropagation();
-		$(this).parent().find('> ul').slideToggle();
-		$(this).parent().children('a').toggleClass('open');
-	});
-	
+    $('#navigation').scrollToFixed();
 });
 
-/* End Navigation Bar JS functions */
+/* End Fixed navigation on scroll function */
 
 /* Typed.js functions */
-
-  
   document.addEventListener('DOMContentLoaded', function(){
-      Typed.new('.element', {
-        strings: ["Front-End Web Developer.", "Graphic Designer.", "Web Developer! &#9996;"],
-        typeSpeed: 100,
-        backDelay: 1000
+      Typed.new('.type-element', {
+        strings: ["Undergraduate IT Student,", "Front-End Web Developer.", "Web Developer! &#9996;"],
+        typeSpeed: 110,
+        backDelay: 400
       });
   });
-
-
 /* End Typed.js functions */
 
+/* Animations on Scroll */
+
+new WOW().init();
+
+/* End Animations on Scroll function */
+
+/* Smooth Scroll function */
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sweetScroll = new SweetScroll({
+        trigger: "[data-scroll]",
+        duration: 1000,
+        delay: 0,
+        easing: "easeInOutCubic",
+        offset: 0,
+        verticalScroll: true,
+        horizontalScroll: false,
+        stopScroll: true,
+        updateURL: false,
+        preventDefault: true,
+        stopPropagation: true,
+        quickMode: false,
+    });
+  }, false);
+
+/* End Smooth Scroll function */
+
 /* Scroll to top function */
-
-
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -168,28 +85,7 @@ function scrollFunction() {
 }
 
 function topFunction() {
-    document.body.scrollTop = 0; //
-    document.documentElement.scrollTop = 0; //
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
-
 /* End Scroll to top function */
-
-/* Random Easter Egg Function */
-
-function myFunction1() {
-    var x = document.getElementById('random-pic1');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
-
-function myFunction2() {
-    var x = document.getElementById('random-pic2');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
